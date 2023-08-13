312660ec5000ce7c431564d3a2ca79d9aa5dafdc
moHaHa <diab.4869@gmail.com>
2023-08-13 02:16:15 +0200

    update : port change

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const URL = 'https://www.footarchives.com/'; // replace with the URL of the website you want to scrape
 
